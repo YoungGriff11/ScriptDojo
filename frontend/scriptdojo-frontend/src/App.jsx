8f31d3705c29e577c3a9e2d8fafaf10ea24ff980
Conor Griffin <115548358+YoungGriff11@users.noreply.github.com>
2025-08-16 10:48:41 +0100

    Frontend libraries installation / Directory refactoring

---
+++ frontend/scriptdojo-frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Editor } from './components/Editor.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>ScriptDojo Editor</h1>
+      <Editor />
+    </div>
+  );
+}
+
+export default App;
